0200d843bb2fb3f616e79db9b8508b732c53812f
AlexeyKhodasevich <powerbandtestalex@gmail.com>
2023-04-27 21:51:14 +0300

    First sign up test added

---
+++ src/test/java/SharelaneRegistrationTest.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SharelaneRegistrationTest {
+    WebDriver driver;
+
+    @Test
+    public void fiveDigitsZipCodeTest() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        // Open register page https://www.sharelane.com/cgi-bin/register.py
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        // Input five digits to zip code field
+        driver.findElement(By.name("zip_code")).sendKeys("12345");
+        // Click Continue button
+        driver.findElement(By.cssSelector("[value='Continue']")).click();
+        // Check Register button
+        WebElement registerButton = driver.findElement(By.cssSelector("[value='Register']"));
+        Assert.assertTrue(registerButton.isDisplayed(), "Register button isn't displayed");
+    }
+}
