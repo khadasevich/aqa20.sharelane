0ba55c684b9d144ba4e622a27d181db981f8ea8b
AlexeyKhodasevich <powerbandtestalex@gmail.com>
2023-05-18 21:21:09 +0300

    lesson 9 TestNG parameters from suite

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
 import utilities.TestListener;
 
@@ -14,20 +15,26 @@ import java.util.concurrent.TimeUnit;
 public class BaseTest {
     protected WebDriver driver;
 
-    @BeforeSuite
-    public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        //опции позволяющие закрывать все блокирующие окна браузера
-        options.addArguments("--disable-notifications");
-        options.setExperimentalOption("excludeSwitches",
-                Arrays.asList("disable-popup-blocking"));
-        driver = new ChromeDriver(options);
+    @BeforeMethod
+    @Parameters({"browser"})
+    public void setUp(@Optional("chrome") String browser) {
+        if (browser.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            //опции позволяющие закрывать все блокирующие окна браузера
+            options.addArguments("--disable-notifications");
+            options.setExperimentalOption("excludeSwitches",
+                    Arrays.asList("disable-popup-blocking"));
+            driver = new ChromeDriver(options);
+        } else if (browser.equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
-    @AfterSuite(alwaysRun = true)
+    @AfterMethod(alwaysRun = true)
     public void tearDown() {
         driver.quit();
     }
